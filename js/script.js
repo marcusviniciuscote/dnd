54841abd19be70ccb489e9645cbb97bfe386b8b6
marcusviniciuscote <marcusviniciuscote@gmail.com>
2025-04-28 20:56:07 -0300

    Subindo funcao para calcular automatico os atributos

--- js/script.js
+++ js/script.js
@@ -1,90 +1,95 @@
 let valorForca = document.getElementById('forca')
+let valorDestreza = document.getElementById('destreza')
+let valorConstituicao = document.getElementById('constituicao')
+let valorInteligencia = document.getElementById('inteligencia')
+let valorSabedoria = document.getElementById('sabedoria')
+let valorCarisma = document.getElementById('carisma')
+
 let valorModificadorForca = document.getElementById('modificadorForca')
+let valorModificadorDestreza = document.getElementById('modificadorDestreza')
+let valorModificadorConstituicao = document.getElementById('modificadorConstituicao')
+let valorModificadorInteligencia = document.getElementById('modificadorInteligencia')
+let valorModificadorSabedoria = document.getElementById('modificadorSabedoria')
+let valorModificadorCarisma = document.getElementById('modificadorCarisma')
 
-
-function calcularModificador() {
-
-    let cm = parseInt(valorForca.value)
-
-    switch (cm) {
+function calcularModificador(valorAtributo, valorModificador) {
+    switch (parseInt(valorAtributo)) {
         case 1:
-            valorModificadorForca.innerHTML = -5
+            novoValorModificador = -5
             break;
         case 2:
         case 3:
-            valorModificadorForca.innerHTML = -4
+            novoValorModificador = -4
             break;
         case 4:
         case 5:
-            valorModificadorForca.innerHTML = -3
+            novoValorModificador = -3
             break;
         case 6:
         case 7:
-            valorModificadorForca.innerHTML = -2
+            novoValorModificador = -2
             break;
         case 8:
         case 9:
-            valorModificadorForca.innerHTML = -1
+            novoValorModificador = -1
             break;
         case 10:
         case 11:
-            valorModificadorForca.innerHTML = 0
+            novoValorModificador = 0
             break;
         case 12:
         case 13:
-            valorModificadorForca.innerHTML = 1
+            novoValorModificador = 1
             break;
         case 14:
         case 15:
-            valorModificadorForca.innerHTML = 2
+            novoValorModificador = 2
             break;
         case 16:
         case 17:
-            valorModificadorForca.innerHTML = 3
+            novoValorModificador = 3
             break;
         case 18:
         case 19:
-            valorModificadorForca.innerHTML = 4
+            novoValorModificador = 4
             break;
         case 20:
         case 21:
-            valorModificadorForca.innerHTML = 5
+            novoValorModificador = 5
             break;
         case 22:
         case 23:
-            valorModificadorForca.innerHTML = 6
+            novoValorModificador = 6
             break;
         case 24:
         case 25:
-            valorModificadorForca.innerHTML = 7
+            novoValorModificador = 7
             break;
         case 26:
         case 27:
-            valorModificadorForca.innerHTML = 8
+            novoValorModificador = 8
             break;
         case 28:
         case 29:
-            valorModificadorForca.innerHTML = 9
+            novoValorModificador = 9
             break;
         case 30:
-            valorModificadorForca.innerHTML = 10
+            novoValorModificador = 10
             break;
         default:
-            valorModificadorForca.innerHTML = 0
+            novoValorModificador = 0
             console.log('passando aqui teste')
     }
-
-
+    valorModificador.innerHTML = parseFloat(novoValorModificador)
 }
 
 function principal() {
 
-
-    calcularModificador()
-    //let newValorModificadorForca = calcularModificador(valorForca.value)
-
-    //return newValorModificadorForca
-
-    //    let testeFuncionando = valorModificadorForca.innerHTML = newValorModificadorForca
+    calcularModificador(valorForca.value, valorModificadorForca)
+    calcularModificador(valorDestreza.value, valorModificadorDestreza)
+    calcularModificador(valorConstituicao.value, valorModificadorConstituicao)
+    calcularModificador(valorInteligencia.value, valorModificadorInteligencia)
+    calcularModificador(valorSabedoria.value, valorModificadorSabedoria)
+    calcularModificador(valorCarisma.value, valorModificadorCarisma)
 
 }
